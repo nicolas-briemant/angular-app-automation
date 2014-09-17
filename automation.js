c696d932fbc1b22744ac26566adbd2d55b65365b
tom-ripley <tom-ripley@users.noreply.github.com>
2014-09-17 12:07:53 +0200

    add dist tasks

--- automation.js
+++ automation.js
@@ -19,24 +19,54 @@ module.exports = function(gulp, options) {
     }
   }
 
-  gulp.task('dev', function(cb) {
+  gulp.task('default', function(cb) {
+    util.log('Available tasks:'
+      + util.colors.green('\ndev') + util.colors.cyan(' lite developement workflow (without tests) ')
+        + '\n- build (browserify, less, sourcemaps, autoprefixer)'
+        + '\n- serve application (express)'
+        + '\n- provide linting (non-blocking) for js (jshint) and css (recess)'
+        + '\n- connected browsers will automatically refresh when files are updated (livereload)'
+      + util.colors.green('\ndist:build') + util.colors.cyan(' distribution building workflow ')
+        + '\n- build (browserify, less, autoprefixer)'
+        + '\n- minify js (uglify2, supports angular DI) and css (csso)'
+      + util.colors.green('\ndist:serve') + util.colors.cyan(' serve distribution for checking purpose')
+    );
+    cb();
+  });
+
+  gulp.task('dev', function() {
     async.series({
-      'clean': clean.bind(null, {src: options.build.dest})
-    , 'build': build.all.bind(null, options)
-    , 'notifier': server.bind(null, {src: options.build.dest})
+      'clean': clean.bind(null, {src: options.dirs.build})
+    , 'build': build.all.bind(null, {dest: options.dirs.build, watch: true}, options)
+    , 'notifier': server.bind(null, {src: options.dirs.build})
     }, function(err, r) {
       if (err) return error(err);
 
       gulp.watch(
         options.css.src
-      , build.css.bind(null, {app: options.css.app, dest: options.build.dest, name: options.name}, null)
+      , build.css.bind(null, {app: options.css.app, dest: options.dirs.build, name: options.name}, null)
       );
       gulp.watch(
         options.html.src
-      , build.html.bind(null, {src: options.html.src, dest: options.build.dest}, null)
+      , build.html.bind(null, {src: options.html.src, dest: options.dirs.build}, null)
       );
 
-      gulp.watch(options.build.dest + '**/*', r.notifier);
+      gulp.watch(options.dirs.build + '**/*', r.notifier);
     });
+  });
+
+  gulp.task('dist:build', function(cb) {
+    async.series([
+      clean.bind(null, {src: options.dirs.dist})
+    , build.all.bind(null, {dest: options.dirs.dist, min: true}, options)
+    ], function(err) {
+      if (err) return error(err);
+      util.log(util.colors.green('Distribution ready in ' + options.dirs.dist));
+      cb();
+    });
+  });
+
+  gulp.task('dist:serve', ['dist:build'], function(cb) {
+    server({src: options.dirs.dist}, cb);
   });
 };
